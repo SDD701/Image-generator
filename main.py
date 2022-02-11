7187cc5912419ce482f276fdbf9ee7d336efbabe
SDD701 <19keriksov@protonmail.com>
2022-02-11 10:40:46 +0700

    algorithm draft

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as mpl
+
+#ALGO:
+#1 IF IT'S UNPAINTED = PAINT IT
+#2 IF IT'S PAINTED = PAINT THE NEXT UNPAINTED PIXEL
+#  AND CLEEAR ALL PIXELS ON THE LEFT TO IT
