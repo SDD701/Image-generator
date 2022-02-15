ab08596e1deadd2dfea2c63639096cb351c70789
SDD701 <19keriksov@protonmail.com>
2022-02-16 01:01:38 +0700

    First test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as mpl
 Widght, Height = 100, 100
 
 Img = np.zeros( (Widght, Height, 3), np.uint8 )
-Widght, Height = Widght-1, Height-1 #because the counting starts from 0
+#Widght, Height = Widght-1, Height-1 #because the counting starts from 0
 Img[:,:] = (255,255,255)
 
 print(Img[10,10])
@@ -19,39 +19,19 @@ Cpixx = 0 #current pixel coordinate x (stands for Current PIXel X)
 Cpixy = 0 #current pixel coordinate y
 Black = False #if all pixels are black?
 WhitePixel = np.array([255,255,255])
-BlackPixel = np.array([0,0,0])
 
 while Black == False:
-    #step 2 part 1/2
-    if np.array_equal(Img[Cpixx, Cpixy], BlackPixel) == True:
-        i = 0
-        j = 0
-        while np.array_equal(Img[Cpixx + i, Cpixy + j], WhitePixel) !=True:
-            if Cpixx + i == Widght:
-                j = j + 1
-                i = -1
-            i = i+1
-        if Cpixx < Widght:
-            Img[Cpixx + i + 1, Cpixy] = (0,0,0)
-        if Cpixx == Widght and Cpixy < Height:
-            Img[Cpixx + i, Cpixy + j + 1] = (0,0,0)
-    #step 2 part 2/2
-        if Cpixy != Height:
-            y = Cpixy
-            for y in range(y, -1, -1):
-                x = Cpixx 
-                for x in range(x, -1, -1):
-                    Img[x, y] = (255,255,255)
+    for Cpixx in range(0, Widght):
+        for Cpixy in range(0, Height):
+            print(Cpixx, Cpixy)
+            if np.array_equal(Img[Cpixx, Cpixy], WhitePixel) == True:
+                Img[Cpixx, Cpixy] = (0,0,0)
+                for Cpixx in range(Cpixx, -1, -1):
+                    for Cpixy in range(Cpixy, -1, -1):
+                        Img[Cpixx, Cpixy] = (0,0,0)
+                        print("a")
+            if Cpixx == Widght - 1 and Cpixy == Height - 1:
+                Black = True
 
-    #step 1
-    if np.array_equal(Img[Cpixx, Cpixy], WhitePixel) == True:
-        Img[Cpixx, Cpixy] = (0,0,0)
-
-    Cpixx = Cpixx + 1
-    if Cpixx == Widght:
-        Cpixx = 0
-        Cpixy = Cpixy + 1
-    #Black = True
-
-    cv2.imshow('image', Img)
-    cv2.waitKey(0)
+    #cv2.imshow('image', Img)
+    #cv2.waitKey(0)
