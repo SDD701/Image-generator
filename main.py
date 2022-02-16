d79f92e62f12dc010670a8892b51e1622a70ef09
SDD701 <19keriksov@protonmail.com>
2022-02-16 15:31:00 +0700

    It's working now

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as mpl
 #2 IF IT'S PAINTED = PAINT THE NEXT UNPAINTED PIXEL
 #  AND CLEAR ALL PIXELS ON THE LEFT TO IT
 
-Widght, Height = 100, 100
+Widght, Height = 200, 200
 
 Img = np.zeros( (Widght, Height, 3), np.uint8 )
 #Widght, Height = Widght-1, Height-1 #because the counting starts from 0
 Img[:,:] = (255,255,255)
 
-print(Img[10,10])
+#print(Img[10,10])
 
 Cpixx = 0 #current pixel coordinate x (stands for Current PIXel X)
 Cpixy = 0 #current pixel coordinate y
@@ -21,17 +21,27 @@ Black = False #if all pixels are black?
 WhitePixel = np.array([255,255,255])
 
 while Black == False:
-    for Cpixx in range(0, Widght):
-        for Cpixy in range(0, Height):
-            print(Cpixx, Cpixy)
+    while Cpixx < Widght:
+        while Cpixy < Height:
+            #step 1 and 2
             if np.array_equal(Img[Cpixx, Cpixy], WhitePixel) == True:
                 Img[Cpixx, Cpixy] = (0,0,0)
-                for Cpixx in range(Cpixx, -1, -1):
-                    for Cpixy in range(Cpixy, -1, -1):
-                        Img[Cpixx, Cpixy] = (0,0,0)
-                        print("a")
-            if Cpixx == Widght - 1 and Cpixy == Height - 1:
-                Black = True
+                #clearing
+                for Cpixy in range(Cpixy - 1, -1, -1):
+                    for Cpixx in range(Cpixx, -1, -1):
+                        Img[Cpixx, Cpixy] = (255,255,255)
+            elif Cpixy < Height - 1:
+                Cpixy = Cpixy + 1
+            else:
+                Cpixy = 0
+                Cpixx = Cpixx +1
+                #check fullness
+                if Cpixx == Widght:
+                    Black = True
+                    Cpixy = Height
+            #output
+            cv2.imshow('image', Img)
+            cv2.waitKey(1)
 
     #cv2.imshow('image', Img)
     #cv2.waitKey(0)
